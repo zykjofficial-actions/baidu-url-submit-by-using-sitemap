1a924c93d2d72bfd195b172c5526071091d5a2c2
zykjofficial <40537396+zykjofficial@users.noreply.github.com>
2020-09-23 10:26:14 +0800

    Update generate.py

--- generate.py
+++ generate.py
@@ -2,7 +2,7 @@ import re
 import urllib
 import requests
 
-sitemap = 'https://blog.zykjofficial.top/baidusitemap.xml'
+sitemap = 'https://blog.zykjofficial.top/sitemap.xml'
 
 html = urllib.request.urlopen(sitemap).read().decode('utf-8')
 result = re.findall(re.compile(r'(?<=<loc>).*?(?=</loc>)'), html)
